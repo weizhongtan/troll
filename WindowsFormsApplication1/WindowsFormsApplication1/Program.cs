e75cc709837d64ff547920fd3f4efa77f634187c
Wei-Zhong Tan <wztan@hotmail.co.uk>
2017-01-15 22:12:58 +0000

    initial commit

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ClipBoardUpdater
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                if (Clipboard.ContainsText())
+                {
+                    var textFromClipboard = Clipboard.GetText();
+
+                    Clipboard.SetText(textFromClipboard.Replace("the", "the bean"));
+                }
+
+                Thread.Sleep(6000); //sleep for a min
+            }
+        }
+    }
+}
